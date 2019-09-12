36e69641a5a2c430071fc41be3f88d50cb8401b5
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-12 14:22:10 -0500

    created html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+//html routes 
+function html(app,path) {
+    app.get('/', function (req, res) {
+        console.log(path.join(__dirname,'../public/home.html'))
+        res.sendFile(path.join(__dirname, '../public/home.html'))
+    })
+
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'))
+    })
+}
+
+module.exports = html;
