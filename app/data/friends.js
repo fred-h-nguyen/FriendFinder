a8a438f13b75fedae76efd84fcec6c993d9abea6
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-15 19:05:47 -0500

    changed some data in array

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://picsum.photos/id/237/200/300",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friends
